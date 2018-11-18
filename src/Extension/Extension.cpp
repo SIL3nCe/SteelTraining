7b304cf5009ff1816ea1157909c14db7d44228dd
Thomas <tom.pradal@aliceadsl.fr>
2018-11-18 18:00:53 +0100

    Added mouse handle in extension

--- src/Extension/Extension.cpp
+++ src/Extension/Extension.cpp
@@ -14,6 +14,10 @@ static register_fn pRegisterST = shNULL;
 static unregister_fn pUnRegisterST = shNULL;
 static get_instance_fn pGetInstanceST = shNULL;
 
+static FuncOnTouchDownPtr pOnTouchDownST = shNULL;
+static FuncOnTouchUpPtr pOnTouchUpST = shNULL;
+static FuncOnTouchMovePtr pOnTouchMoveST = shNULL;
+
 #ifdef PLUGIN_ST_PATH
 static const CShString strPluginSTFilePath(PLUGIN_ST_PATH);
 #else
@@ -53,6 +57,15 @@ EXTENSION_EXPORT void ExtensionOnPluginStarted(const CShIdentifier & levelIdenti
 
 		pGetInstanceST = (get_instance_fn)pluginST_library.GetFunctionAddress(CShString("GetPlugin"));
 		SH_ASSERT(shNULL != pGetInstanceST);
+
+		pOnTouchDownST = (FuncOnTouchDownPtr)pluginST_library.GetFunctionAddress(CShString("TouchDownPluginST"));
+		SH_ASSERT(shNULL != pOnTouchDownST);
+
+		pOnTouchUpST = (FuncOnTouchUpPtr)pluginST_library.GetFunctionAddress(CShString("TouchUpPluginST"));
+		SH_ASSERT(shNULL != pOnTouchUpST);
+
+		pOnTouchMoveST = (FuncOnTouchMovePtr)pluginST_library.GetFunctionAddress(CShString("TouchMovePluginST"));
+		SH_ASSERT(shNULL != pOnTouchMoveST);
 	}
 
 	//
@@ -60,6 +73,10 @@ EXTENSION_EXPORT void ExtensionOnPluginStarted(const CShIdentifier & levelIdenti
 	if (shNULL != pRegisterST)
 	{
 		pRegisterST();
+
+		ShInput::AddOnTouchDown(pOnTouchDownST);
+		ShInput::AddOnTouchUp(pOnTouchUpST);
+		ShInput::AddOnTouchMove(pOnTouchMoveST);
 	}
 }
 
@@ -69,6 +86,10 @@ EXTENSION_EXPORT void ExtensionOnPluginStopped(const CShIdentifier & levelIdenti
 	// Call factory (auto-unregister)
 	if (shNULL != pUnRegisterST)
 	{
+		ShInput::RemoveOnTouchDown(pOnTouchDownST);
+		ShInput::RemoveOnTouchUp(pOnTouchUpST);
+		ShInput::RemoveOnTouchMove(pOnTouchMoveST);
+
 		pUnRegisterST();
 	}
 
@@ -84,4 +105,5 @@ EXTENSION_EXPORT void ExtensionOnPluginStopped(const CShIdentifier & levelIdenti
 	// Unload Library
 	pluginST_library.Release();
 }
+
 }
