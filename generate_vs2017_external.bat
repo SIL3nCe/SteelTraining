5b7846da6ee2f8a7d1d3f0a2263e972c7ca9fc48
Thomas <tom.pradal@aliceadsl.fr>
2019-01-08 21:30:30 +0100

    Updated vs2017 bat
    Fixed _Editor lib link

--- generate_vs2017_external.bat
+++ generate_vs2017_external.bat
@@ -1,13 +1,14 @@
-set BUILD_DIR=build
+set BUILD_DIR=buildExternal2017
 
-set EDITOR_EXT_DIR=C:\ShineEngine\bin
+set EDITOR_EXT_DIR=C:\ShineResearch\ShineEngine\bin
+set SDK_EXT_DIR=C:\ShineResearch\ShineEngine\lib
 
 set GENERATOR="Visual Studio 15 2017"
 
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-	cmake -G %GENERATOR% -DSHINE_EDITOR_EXT_DIR=%EDITOR_EXT_DIR% -DSHINE_INTERNAL=0 -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
+	cmake -G %GENERATOR% -DSHINE_EDITOR_EXT_DIR=%EDITOR_EXT_DIR% -DSHINE_INTERNAL=0 -DSHSDK_LIBRARIES="%SDK_EXT_DIR%/ShSDK.lib;%SDK_EXT_DIR%/ShCore.lib" -DSHSDK_EDITOR_LIBRARIES="%SDK_EXT_DIR%/ShSDK_Editor.lib;%SDK_EXT_DIR%/ShCore_Editor.lib" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
 
 cd ..
 pause
