a4b790e21fa30435f0147f0f5e1a1fcdfe273e02
Johann NOVAK <johann.novak25@gmail.com>
2019-03-30 12:44:28 +0100

    Fixed link errors + added Debug/Master support

--- src/Game/Application.cpp
+++ src/Game/Application.cpp
@@ -4,11 +4,6 @@
 #include "PluginFactory.h"
 #include "PluginMapGeneratorFactory.h"
 
-#if SH_PC
-#	include <winsock2.h>
-#	include <ws2tcpip.h>
-#endif // SH_PC
-
 extern "C"
 {
 
@@ -17,26 +12,6 @@ extern "C"
  */
 void OnPreInitialize(void)
 {
-#if SH_PC
-	WSADATA wsaData;
-
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
-		SH_TRACE(">>> Error initializing Winsock 2.2");
-		WSACleanup();
-		return;
-	}
-
-	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-	{
-		SH_TRACE(">>> Winsock 2.2 not available");
-		WSACleanup();
-		return;
-	}
-
-	SH_TRACE("\nWinsock 2.2 initialized via wsa2_32.dll");
-#endif // SH_PC
-
 	ShInput::AddOnTouchDown(OnTouchDown);
 	ShInput::AddOnTouchUp(OnTouchUp);
 	ShInput::AddOnTouchMove(OnTouchMove);
@@ -93,9 +68,7 @@ void OnPreRelease(void)
  */
 void OnPostRelease(void)
 {
-#if WIN32
-	WSACleanup();
-#endif
+	// ...
 }
 
 /**
