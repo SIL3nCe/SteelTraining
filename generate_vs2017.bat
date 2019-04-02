15e054667f7e637318f50388d339fc7c45c1735c
S1L3nCe <tom.pradal@aliceadsl.fr>
2019-04-02 11:36:47 +0200

    Fixed 2017 bat

--- generate_vs2017.bat
+++ generate_vs2017.bat
@@ -2,14 +2,14 @@ SET BUILD_DIR=build2017
 
 SET DEVLINE_DIR=C:/ShineP4/Shine/MainLine
 
-SET SHSDK_DIR=%DEVLINE_DIR%/Build/Win32_VS2015/Tools/Libraries/Internal/
+SET SHSDK_DIR=%DEVLINE_DIR%/Build/Win32_VS2017/Tools/Libraries/Internal/
 
 set GENERATOR="Visual Studio 15 2017"
 
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-	cmake -G %GENERATOR% -DShSDK_DIR="%DEVLINE_DIR%/Build/Win32_VS2015/cmake/lib/x86/ShSDK" -DSHSDK_EDITOR_LIBRARIES="%SHSDK_DIR%/ShSDK/Debug/ShSDK_Editor_Debug.lib;%SHSDK_DIR%/ShCore/Debug/ShCore_Editor_Debug.lib" -DSHINE_INTERNAL=1 -DSHINE_EDITOR_EXT_DIR="%DEVLINE_DIR%/Build/Win32_VS2015/Compiled/Bin/x86/Extensions" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
+	cmake -G %GENERATOR% -DShSDK_DIR="%DEVLINE_DIR%/Build/Win32_VS2017/cmake/lib/x86/ShSDK" -DSHSDK_EDITOR_LIBRARIES="%SHSDK_DIR%/ShSDK/Debug/ShSDK_Editor_Debug.lib;%SHSDK_DIR%/ShCore/Debug/ShCore_Editor_Debug.lib" -DSHINE_INTERNAL=1 -DSHINE_EDITOR_EXT_DIR="%DEVLINE_DIR%/Build/Win32_VS2015/Compiled/Bin/x86/Extensions" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
 
 cd ..
 pause
