ceb5f2ddf373f5b793fb8b463aaed4b71317e9b0
Johann NOVAK <johann.novak25@gmail.com>
2019-05-07 23:31:35 +0200

    Fixed ShSDK_Editor find + switch debug/master

--- Generate_Linux_Codeblocks_Makefiles.sh
+++ Generate_Linux_Codeblocks_Makefiles.sh
@@ -1,7 +1,6 @@
 BUILD_DIR=Linux_CodeBlocks_Makefiles
 
 DIR=`pwd`
-EDITOR_EXT_DIR=${SHINE_BASE_DIR}/bin/extentions
 GENERATOR="CodeBlocks - Unix Makefiles"
 
 mkdir build
@@ -16,7 +15,7 @@ cd build
 			mkdir ${CONFIG}
 			cd ${CONFIG}
 
-				cmake -G "${GENERATOR}" -DSHINE_EDITOR_EXT_DIR="${EDITOR_EXT_DIR}" -DSHINE_INTERNAL=0 -DCMAKE_BUILD_TYPE="${CONFIG}" ${DIR}
+				cmake -G "${GENERATOR}" -DSHINE_INTERNAL=0 -DCMAKE_BUILD_TYPE="${CONFIG}" ${DIR}
 
 			cd ..
 
--- generate_vs2017_external.bat
+++ generate_vs2017_external.bat
@@ -1,15 +1,13 @@
 set BUILD_DIR=buildExternal2017
 
-set EDITOR_EXT_DIR=J:\ShineResearch\ShineEngine\bin
-set SDK_EXT_DIR=J:\ShineResearch\ShineEngine\lib
-set SDK_EXT_ROOT=J:\ShineResearch\ShineEngine
+set SHINE_EXTERNAL_ROOT=W:/shine_external
 
 set GENERATOR="Visual Studio 15 2017"
 
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-	cmake -G %GENERATOR% -DSHINE_EDITOR_EXT_DIR=%EDITOR_EXT_DIR% -DSHINE_INTERNAL=0 -DSHSDK_EDITOR_LIBRARIES_MASTER="optimized;%SDK_EXT_DIR%/ShSDK_Editor.lib;optimized;%SDK_EXT_DIR%/ShCore_Editor.lib" -DSHSDK_EDITOR_LIBRARIES_DEBUG="debug;%SDK_EXT_DIR%/ShSDK_Editor_Debug.lib;debug;%SDK_EXT_DIR%/ShCore_Editor_Debug.lib" -DSHSDK_EDITOR_LIBRARIES="%SDK_EXT_DIR%/ShSDK_Editor_Debug.lib;%SDK_EXT_DIR%/ShCore_Editor_Debug.lib" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
+	cmake -G %GENERATOR% -DSHINE_INTERNAL=0 -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
 
 cd ..
 pause
