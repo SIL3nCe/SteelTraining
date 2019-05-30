c11308ded4a17c18ed5de95c4860c73db02b1208
Johann NOVAK <johann.novak25@gmail.com>
2019-05-30 13:42:11 +0200

    Update generate_vs2017_d3d11.bat

--- generate_vs2017_d3d11.bat
+++ generate_vs2017_d3d11.bat
@@ -9,7 +9,7 @@ set GENERATOR="Visual Studio 15 2017"
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-	cmake -G %GENERATOR% -DShSDK_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK" -DSHSDK_EDITOR_LIBRARIES_DEBUG="%SHSDK_DIR%/ShSDK/Debug/ShSDK_Editor_Debug.lib;%SHSDK_DIR%/ShCore/Debug/ShCore_Editor_Debug.lib;%SHSDK_DIR%/ShCompiler/Debug/ShCompiler_Editor_Debug.lib"  -DSHSDK_EDITOR_LIBRARIES_MASTER="%SHSDK_DIR%/ShDK/Master/ShSDK_Editor.lib;%SHSDK_DIR%/ShCore/Master/ShCore_Editor.lib;%SHSDK_DIR%/ShCompiler/Master/ShCompiler_Editor.lib" -DSHINE_INTERNAL=1 -DSHINE_EDITOR_EXT_DIR="%DEVLINE_DIR%/Build/Win32_VS2017/Compiled/Bin/x86/Extensions" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
+	cmake -G %GENERATOR% -DShSDK_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK" -DShSDK_Editor_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK_Editor" -DSHINE_INTERNAL=1 -DSHINE_EDITOR_EXT_DIR="%DEVLINE_DIR%/Build/Win32_VS2017/Compiled/Bin/x86/Extensions" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
 
 cd ..
 pause
