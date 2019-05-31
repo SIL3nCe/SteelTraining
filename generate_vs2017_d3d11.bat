327dc27a7cd54d9aa51ee43afe3b3683d54bcc82
Johann NOVAK <johann.novak25@gmail.com>
2019-05-31 23:04:46 +0200

    Update project according to ShSDKEditor - cleaned

--- generate_vs2017_d3d11.bat
+++ generate_vs2017_d3d11.bat
@@ -9,7 +9,7 @@ set GENERATOR="Visual Studio 15 2017"
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-	cmake -G %GENERATOR% -DShSDK_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK" -DShSDK_Editor_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK_Editor" -DSHINE_INTERNAL=1 -DSHINE_EDITOR_EXT_DIR="%DEVLINE_DIR%/Build/Win32_VS2017/Compiled/Bin/x86/Extensions" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
+	cmake -G %GENERATOR% -DShSDK_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK" -DShSDKEditor_DIR="%DEVLINE_DIR%/Build/Win32_x86_VS2017_DX11/cmake/lib/x86/ShSDK_Editor" -DSHINE_INTERNAL=1 -DSHINE_EDITOR_EXT_DIR="%DEVLINE_DIR%/Build/Win32_VS2017/Compiled/Bin/x86/Extensions" -DCMAKE_CONFIGURATION_TYPES=Debug;Release;Master ..
 
 cd ..
 pause
